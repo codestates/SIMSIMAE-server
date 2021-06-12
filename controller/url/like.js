edd9cd76e91099c5761cfeace62199c80d5380e5
hyebin <dlgpqls93@gmail.com>
2021-06-13 02:09:52 +0900

    :sparkles: add accesstoken, refreshtoken, like api code

--- controller/url/like.js
+++ controller/url/like.js
@@ -1,7 +1,22 @@
+const { Url } = require('../../models');
+const { Likes } = require('../../models');
+
 module.exports = async (req, res) => {
+    //const {email, url} = req.body;
+    const { userId, url } = req.body;
 
-    const {email, url} = req.body;
-
-    res.send({email, url})
-
+    //좋아요 누르면 url 테이블에서 url과 일치하는 id 찾아와서
+    //likes 테이블에 넣기
+    const urlId = await Url.findOne({
+        where: {url: url},
+        attributes: ['id']
+    })
+    //console.log('urlId', urlId.id);
+    await Likes.create({
+        user_id: userId,
+        url_id: urlId
+    })
+    res.send({
+        message: 'success'
+    })
 }
--- controller/users/refreshTokenRequest.js
+++ controller/users/refreshTokenRequest.js
@@ -1,3 +1,33 @@
-module.exports = async (req, res) => {
+const { User } = require('../../models');
+const jwt = require('jsonwebtoken');
 
+module.exports = async (req, res) => {
+    const { refreshToken } = req.cookies;
+    console.log('resfresh', refreshToken)
+    if(!refreshToken) {
+        res.status(400).send('refresh token not provided');
+    } else {
+        const data = jwt.verify(refreshToken, process.env.REFRESH_SECRET, (err, decoded) => {
+            if(err) {
+                res.status(403).send('invalid refresh token, please log in again')
+            } else {
+                return decoded
+            }
+        }) 
+
+        const userInfo = await User.findOne({
+            where : {email: data.email}
+        })
+        if(!userInfo) {
+            res.status(404).send('refresh token has been tempered')
+        } else {
+            delete userInfo.dataValues.password;
+            let payload = {...userInfo.dataValues}
+            const accessToken = jwt.sign(payload, process.env.ACCESS_SECET)
+            res.send({            
+                accessToken: accessToken,
+                userInfo: userInfo.dataValues
+            })
+        }
+    }
 }
