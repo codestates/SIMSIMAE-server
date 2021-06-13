c032729fa688ce9e028ccad1a80da7e00cac1a26
nara yoo <younara3106@gmail.com>
2021-06-14 08:52:46 +0900

    :bulb: add comments in source code /controller-signUp.js

--- controller/users/signUp.js
+++ controller/users/signUp.js
@@ -14,6 +14,8 @@ module.exports = async (req,res) => {
 
       })
     const conflictMail = await User.findOne({ where: { email }})
+
+    const userLikes = await User.findOrCreate()
      
     if(createUser){
          res.status(201).send({email, name, phone}) 
@@ -26,4 +28,8 @@ module.exports = async (req,res) => {
 
 }  
 
+
+// 회원가입완료 후 / 관심사 체크 > 분기점을 만들어야하지않을까? 
+// 사인업을하고 userlike라는 히스토리로 이동하게한 뒤 거기서 만들어진 유저에 추가적으로 데이터를 넣어주고 
+// 카테고리에도 아이디를 넣게끔..? 찾아서 없으면 생성해주는 findOrCreate를 써주면 될 것같다. 
 
