ebeb0e8214fd0d79e1d88c3ea6bd63a215d3d6db
hyebin <dlgpqls93@gmail.com>
2021-06-13 16:37:10 +0900

    :memo: update like and dislike

--- controller/url/like.js
+++ controller/url/like.js
@@ -1,10 +1,14 @@
 const { Url } = require('../../models');
-const { Likes } = require('../../models');
+const { Dislikes } = require('../../models');
+const { User } = require('../../models');
 
 module.exports = async (req, res) => {
-    //const {email, url} = req.body;
-    const { userId, url } = req.body;
+    const {email, url} = req.body;
 
+    const userId = await User.findOne({
+        where: {email: email},
+        attributes: ['id']
+    })
     //좋아요 누르면 url 테이블에서 url과 일치하는 id 찾아와서
     //likes 테이블에 넣기
     const urlId = await Url.findOne({
@@ -12,7 +16,7 @@ module.exports = async (req, res) => {
         attributes: ['id']
     })
     //console.log('urlId', urlId.id);
-    await Likes.create({
+    await Dislikes.create({
         user_id: userId,
         url_id: urlId
     })
