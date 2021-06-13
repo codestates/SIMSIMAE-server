8a0390e737f1447105ffa58ee98952f6a318d3b4
nara yoo <younara3106@gmail.com>
2021-06-13 16:58:27 +0900

    :memo: creating dislike drop and modify signup and model User document

--- controller/url/dislike.js
+++ controller/url/dislike.js
@@ -1,5 +1,27 @@
-module.exports = async (req, res) => {
-    const {email, url} =req.body;
-    res.send({email, url})
+const { Url } = require('../../models');
+const { Likes } = require('../../models');
+const { User } = require('../../models')
 
+module.exports = async (req, res) => {
+    const { email , url } = req.body;
+
+    //싫어요 누르면 url 테이블에서 url과 일치하는 id 찾아와서
+    //DisLikes 테이블에 넣기
+    const urlId = await Url.findOne({
+        where: {url: url},
+        attributes: ['id']
+    })
+
+    const userId = await User.findOne({
+        where : {email : email},
+        attributes : ['id']
+    })
+
+    await DisLikes.create({
+        user_id: userId,
+        url_id: urlId
+    })
+    res.send({
+        message: 'success'
+    })
 }
--- controller/users/logout.js
+++ controller/users/logout.js
@@ -1,3 +1,9 @@
 module.exports = async (req, res) => {
     res.send('로그아웃이 완료되었습니다')
 }
+
+
+// /logout endpoint로 accesstoken을 서버로 보낸다 
+// 서버에서 유저 확인이되면 메세지만 null 값으로 바꾸라는 응답을 보낸다 
+// 실제 유저의 accesstoken 상태 업데이트는 클라이언트에서 null 값으로 바꿔준다. 
+//  
